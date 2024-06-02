06a2792e1e3f10e3d0b6c831000884401c9bd597
awsomeakash <akashpanda29@gmail.com>
2024-06-02 09:02:24 +0530

    Added constants.js to maintain the category

---
+++ App/src/Utils/Constants.ts
@@ -0,0 +1,10 @@
+const questionTypeSelect = ['Multiplication','Addition-Subtraction','Division','Playgorund'] 
+
+const questionTypeSelectMapper = {
+    'Multiplication': 'Multiplication',
+    'Addition-Subtraction': 'Addition-Subtraction',
+    'Division': 'Division',
+    'Playgorund': 'PlaygroundHomePage',
+}
+
+export { questionTypeSelect, questionTypeSelectMapper }
